2e52eba5ae22ba3b32783a7677d6f66f00e97199
ninjaggobugi <ninjaggobugi@naver.com>
2026-08-19 18:42:46 +0900

    feat : 구글 애널리틱스 태그 관리자 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
 import { Navbar } from '@/components/Navbar';
+import { GoogleTagManager } from '@next/third-parties/google';
 import './globals.css';
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
@@ -31,6 +32,7 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
+        <GoogleTagManager gtmId="GTM-TJCRQ5X6" />
       </body>
     </html>
   );
